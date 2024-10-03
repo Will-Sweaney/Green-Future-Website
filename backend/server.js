56250b2dcb1b52ef7b78bc3ffa37de96c0480736
WILLATRONIX <141579350+Will-Sweaney@users.noreply.github.com>
2024-10-03 23:17:50 +0100

    init

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('green_future_db', 'root', 'thEro0t34', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+app.use(express.json()); // Middleware to parse JSON requests
+
+app.get('/', (req, res) => {
+  res.send('get good');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
